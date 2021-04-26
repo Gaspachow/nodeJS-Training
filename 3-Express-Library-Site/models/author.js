3d98cf6a4ba6240e7a5e31ee46ae7895c22642a3
gaspachow <gaspardsmets@gmail.com>
2021-04-26 20:08:46 +0200

    author model

---
+++ 3-Express-Library-Site/models/author.js
@@ -0,0 +1,30 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let AuthorSchema = new Schema(
+    {
+        first_name: {type: String, required: true, maxLength: 100},
+        family_name: {type: String, required: true, maxLength: 100},
+        date_of_birth: {type: Date},
+        date_of_death: {type: Date},
+    }
+);
+
+// Virtual for author's full name
+AuthorSchema.virtual('name').get(() => {
+    return (this.family_name + ', ' + this.first_name);
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual('lifespan').get(() => {
+    return ((this.date_of_death.getYear() - this.date_of_birth.getYear()).toString());
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual('url').get(() => {
+    return ('/catalog/author/' + this._id);
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
