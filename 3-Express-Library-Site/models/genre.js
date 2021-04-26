981d799988b434212aa800cc263d64b0f81428d7
gaspachow <gaspardsmets@gmail.com>
2021-04-26 20:25:26 +0200

    Genre model

---
+++ 3-Express-Library-Site/models/genre.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let Genre = new Schema(
+    {
+        name: {type: String, required: true, minLength: 3, maxLength: 100},
+    }
+);
+
+// Virtual for bookInstance's URL
+Genre.virtual('url').get(() => {
+    return ('/catalog/genre/' + this._id);
+});
+
+// Export model
+module.exports = mongoose.model('Genre', Genre);
