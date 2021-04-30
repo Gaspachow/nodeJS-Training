716879d2ba10be792786ddc93c0acfd7224186d7
gaspachow <gaspardsmets@gmail.com>
2021-04-30 16:15:15 +0200

    Book list page

--- 4-Express-Library-Site/controllers/bookController.js
+++ 4-Express-Library-Site/controllers/bookController.js
@@ -29,7 +29,14 @@ exports.index = function(req, res) {
 
 // Display list of all books.
 exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book list');
+    Book.find({}, 'title author')
+        .populate('author')
+        .exec((err, list_books) => {
+            if (err)
+                return (next(err));
+            res.render('book_list', {title: 'Book List', book_list: list_books });
+            console.log(list_books);
+        });
 };
 
 // Display detail page for a specific book.
